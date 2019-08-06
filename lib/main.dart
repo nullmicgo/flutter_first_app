4b4baac6d3e9292808033bf67eebfd6d5f1a0051
nullmicgo <nullmicog@gmail.com>
2019-08-06 19:56:29 +0800

    trying set State

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    //TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer chosen!');
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
   }
 
   @override
@@ -21,18 +34,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The questions!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: () => print('Answer 2 chosen!'),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: () => print('Answer 3 chosen!'),
             ),
           ],
         ),
